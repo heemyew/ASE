594d29e86d002e4cf0a898020c040c8a6e842d14
heemyew <49015870+heemyew@users.noreply.github.com>
2020-03-11 21:56:41 +0800

    sample

---
+++ Sample/Face-Detection-Based-Attendance-System-master/ConnectionString.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+
+namespace MultiFaceRec
+{
+   public class ConnectionString
+    {
+        public string DBConn = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\heem\Desktop\ASE stuff\Face-Detection-Based-Attendance-System-master\Attendance.mdf;Integrated Security=True";
+
+        //public string DBConn = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=E:\EDUCATION\THEIRD YEAR\3rd year 2nd term\LAB\CSE-Web Programming Project-3200\FaceRecProOV (1)\FaceRecProOV\FaceRecProOV\FaceRecProOV\Attendance.mdf;Integrated Security=True";
+    }
+}
